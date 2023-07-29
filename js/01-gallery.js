3cd52732f230a822c46f054e99658e65f199f59e
Iryna <ira.shkraba@ukr.net>
2023-07-29 22:37:25 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,16 +5,32 @@ import { galleryItems } from "./gallery-items.js";
 
 const containerPhotos = document.querySelector(".gallery");
 
+containerPhotos.addEventListener("click", handleClick);
+
 const addPhotos = galleryItems
   .map(
     (photo) =>
       `<li class ="gallery__item">
     <a class ="gallery__link" href ="${photo.original}">
     <img class ="gallery__image"
-    src ="${photo.preview}" alt ="${photo.description}" loading ="lazy" />
+    src ="${photo.preview}" data-source = "${photo.original}" alt ="${photo.description}" loading ="lazy" />
     </a>
   </li>`
   )
   .join("");
 
 containerPhotos.insertAdjacentHTML("beforeend", addPhotos);
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  } else {
+    const currentUrl = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${currentUrl}" width="800" height="600">
+`);
+
+    instance.show();
+  }
+}
