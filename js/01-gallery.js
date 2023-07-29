66aef9df37001fd6e76b943e7c3865f9c59acdd6
Iryna <ira.shkraba@ukr.net>
2023-07-29 21:25:49 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const containerPhotos = document.querySelector(".gallery");
+
+const addPhotos = galleryItems
+  .map(
+    (photo) =>
+      `<li class ="gallery__item">
+    <a class ="gallery__link" href ="${photo.original}">
+    <img class ="gallery__image"
+    src ="${photo.preview}" alt ="${photo.description}" loading ="lazy" />
+    </a>
+  </li>`
+  )
+  .join("");
+
+containerPhotos.insertAdjacentHTML("beforeend", addPhotos);
