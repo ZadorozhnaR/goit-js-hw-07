67871586e9679891da95899b51a7f85342c2cacf
Iryna <ira.shkraba@ukr.net>
2023-07-30 14:27:08 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
 const containerPhotos = document.querySelector(".gallery");
 
 const addPhotos = galleryItems
